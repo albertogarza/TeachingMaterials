c756701f46d8946c76e24336a22a30a3c4d8f996
Matc <Matc@Matcs-Mac-mini.local>
2015-12-15 10:45:17 -0700

    End of class check in

--- AngularJS/09-Filters/js/homeController.js
+++ AngularJS/09-Filters/js/homeController.js
@@ -4,15 +4,16 @@
     angular.module('homeController', [])
         .controller('homeController', homeController);
 
-    homeController.$inject = [];
+    homeController.$inject = ['$filter'];
 
-    function homeController() {
+    function homeController($filter) {
 
         // list everything
         var hc = this;
         hc.costOfToothbrush = 2.00;
         hc.marbleCount = 1522;
         hc.aloneDate = new Date(1990, 11, 25);
+        hc.formattedDate=$filter('date')(hc.aloneDate,'yyyy-MM-dd');
         hc.contacts = [
             {name:'Kevin', phone:'847-555-8761'},
             {name:'Buzz', phone:'847-555-8762'},
